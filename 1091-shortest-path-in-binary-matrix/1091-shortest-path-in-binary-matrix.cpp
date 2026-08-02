3d6f0d2bfbb97ad65010e23e1918609f0dcaa4e5
Yash007-1 <yash116765@gmail.com>
2026-08-02 15:49:44 +0530

    Time: 34 ms (23.43%), Space: 23.5 MB (92.76%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -6,23 +6,23 @@ public:
         int m=grid[0].size();
         if(grid[0][0]==1||grid[n-1][m-1]==1)return -1;
         priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>pq;
-        vector<vector<int>>dis(n,vector<int>(m,1e9));
-        dis[0][0]=0;
-        pq.push({0,{0,0}});
+        vector<vector<int>>vis(n,vector<int>(m,1e9));
+        vis[0][0]=1;
+        pq.push({1,{0,0}});
         while(!pq.empty()){
             int r=pq.top().second.first;
             int c=pq.top().second.second;
             int length=pq.top().first;
             pq.pop();
-            if(r==n-1&&c==m-1)return length+1;
+            if(r==n-1&&c==m-1)return length;
             for(int i=-1;i<=1;i++){
                 for(int j=-1;j<=1;j++){
                     int nr=r+i;
                     int nc=c+j;
                     if(nr>=0&&nr<n&&nc>=0&&nc<m&&grid[nr][nc]==0){
-                        if(length+1<dis[nr][nc]){
+                        if(length+1<vis[nr][nc]){
                             pq.push({length+1,{nr,nc}});
-                            dis[nr][nc]=length+1;
+                            vis[nr][nc]=length+1;
                         }
                     }
                 }
