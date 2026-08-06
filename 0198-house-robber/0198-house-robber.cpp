56f87240d964fcd217c39c93b3a5079cfff68e47
Yash007-1 <yash116765@gmail.com>
2026-08-06 22:00:30 +0530

    Time: 0 ms (100%), Space: 10.7 MB (24.33%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -10,7 +10,14 @@ public:
       }
     int rob(vector<int>& nums) {
         vector<int>dp(nums.size(),-1);
-
-       return helper(nums,nums.size()-1,dp);
+        dp[0]=nums[0];
+        for(int i=1;i<nums.size();i++){
+            int take=nums[i];
+            if(i>1)take=nums[i]+dp[i-2];
+            int n_take=dp[i-1];
+            dp[i]=max(take,n_take);
+        }
+        
+      return dp[nums.size()-1];
     }
 };
