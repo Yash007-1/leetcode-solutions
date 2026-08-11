f9eb1c3017298bc8f5d53131350fcf26f37bea21
Yash007-1 <yash116765@gmail.com>
2026-08-11 19:02:44 +0530

    Time: 47 ms (35.2%), Space: 48.8 MB (42.14%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -13,9 +13,23 @@ int f(vector<int>&coins,int idx,int amt,vector<vector<int>>&dp){
     return dp[idx][amt]=min(take,ntake);
 }
     int coinChange(vector<int>& coins, int amount) {
-        vector<vector<int>>dp(coins.size(),vector<int>(amount+1,-1));
-        int ans=f(coins,coins.size()-1,amount,dp);
+        vector<vector<int>>dp(coins.size(),vector<int>(amount+1,INT_MAX-1));
+        
+        for(int i=0;i<coins.size();i++)dp[i][0]=0;
+       for(int i=0;i<=amount;i++){
+        if(i%coins[0]==0)dp[0][i]=i/coins[0];
+       }
+        for(int i=1;i<coins.size();i++){
+            for(int j=1;j<=amount;j++){
+                int ntake=dp[i-1][j];
+                int take=INT_MAX;
+                if(j>=coins[i])take=1+dp[i][j-coins[i]];
+                dp[i][j]=min(take,ntake);
+            }
+        }
+        int ans=dp[coins.size()-1][amount];
         if(ans==INT_MAX-1)return -1;
-        return ans;
+
+       return ans ;
     }
 };
