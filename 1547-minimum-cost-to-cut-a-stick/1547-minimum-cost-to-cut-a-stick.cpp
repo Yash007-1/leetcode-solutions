5cae5045f0e3be97057ff2015342b462bd2b27b7
Yash007-1 <yash116765@gmail.com>
2026-08-25 16:09:20 +0530

    Time: 15 ms (93.87%), Space: 14.1 MB (8.76%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -1,23 +1,42 @@
 class Solution {
 public:
-int f(vector<int>&cuts,int i,int j,vector<vector<int>>&dp){
-    if(j-i<=1)return 0;
-    if(dp[i][j]!=-1)return dp[i][j];
-    int minc=1e9;
-    for(int k=i+1;k<j;k++){
-        int left=f(cuts,i,k,dp);
-        int right=f(cuts,k,j,dp);
-        minc=min(minc,left+right+cuts[j]-cuts[i]);    }
-    return dp[i][j]= minc;
-}
+    int f(vector<int>& cuts, int i, int j, vector<vector<int>>& dp) {
+        if (j - i <= 1)
+            return 0;
+        if (dp[i][j] != -1)
+            return dp[i][j];
+        int minc = 1e9;
+        for (int k = i + 1; k < j; k++) {
+            int left = f(cuts, i, k, dp);
+            int right = f(cuts, k, j, dp);
+            minc = min(minc, left + right + cuts[j] - cuts[i]);
+        }
+        return dp[i][j] = minc;
+    }
     int minCost(int n, vector<int>& cuts) {
-       
-        vector<int>arr=cuts;
+
+        vector<int> arr = cuts;
         arr.push_back(n);
         arr.push_back(0);
-        sort(arr.begin(),arr.end());
-        int m=arr.size();
-         vector<vector<int>>dp(m,vector<int>(m,-1));
-        return f(arr,0,m-1,dp);
+        sort(arr.begin(), arr.end());
+        int m = arr.size();
+        vector<vector<int>> dp(m, vector<int>(m, 1e9));
+        for (int i = 0; i < m - 1; i++) {
+            dp[i][i] = 0;
+            dp[i][i + 1] = 0;
+        }
+        dp[m - 1][m - 1] = 0;
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = i + 2; j < m; j++) {
+                int minc = 1e9;
+                for (int k = i + 1; k < j; k++) {
+                    int left = dp[i][k];
+                    int right = dp[k][j];
+                    minc = min(minc, left + right + arr[j] - arr[i]);
+                }
+                dp[i][j] = minc;
+            }
+        }
+        return  dp[0][m - 1];
     }
 };
