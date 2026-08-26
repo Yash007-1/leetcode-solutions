306164a19c660fd2bbdfecf16665d7ef82230d86
Yash007-1 <yash116765@gmail.com>
2026-08-26 23:27:50 +0530

    Time: 212 ms (12.43%), Space: 14 MB (31.24%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
-    int f(vector<int>&nums,int i,int j,vector<vector<int>>&dp){
-        
-       if(i>j)return 0; 
-       if(dp[i][j]!=-1)return dp[i][j];
-        int mx=0;
-        for(int k=i;k<=j;k++){
-            int left=f(nums,i,k-1,dp);
-            int right=f(nums,k+1,j,dp);
-            mx=max(mx,left+right+nums[i-1]*nums[k]*nums[j+1]);
+    int f(vector<int>& nums, int i, int j, vector<vector<int>>& dp) {
+
+        if (i > j)
+            return 0;
+        if (dp[i-1][j-1] != -1)
+            return dp[i-1][j-1];
+        int mx = 0;
+        for (int k = i; k <= j; k++) {
+            int left = f(nums, i, k - 1, dp);
+            int right = f(nums, k + 1, j, dp);
+            mx = max(mx, left + right + nums[i - 1] * nums[k] * nums[j + 1]);
         }
-        return dp[i][j]= mx;
+        return dp[i-1][j-1] = mx;
     }
     int maxCoins(vector<int>& nums) {
-      
-        nums.insert(nums.begin(),1);
+  int n = nums.size();
+        nums.insert(nums.begin(), 1);
         nums.push_back(1);
-         int n=nums.size();
-          vector<vector<int>>dp(n,vector<int>(n,-1));
-        return f(nums,1,n-2,dp);
+      
+        vector<vector<int>> dp(n, vector<int>(n, -1));
+        
+        return f(nums, 1, n, dp);
     }
 };
