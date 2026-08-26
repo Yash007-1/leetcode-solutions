553497d63e8f13a4dbc43b475f813cf9787a3bc4
Yash007-1 <yash116765@gmail.com>
2026-08-26 17:16:33 +0530

    Time: 3 ms (26.7%), Space: 8.8 MB (45.09%) - LeetHub

--- 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -1,48 +1,46 @@
 class Solution {
 public:
     string shortestBeautifulSubstring(string s, int k) {
-        unordered_map<char, int> m;
+
         int n = s.size();
-int ones=0;
+        int ones = 0;
         string ans;
         for (int i = 0; i < n; i++)
-            if(s[i]=='1')ones++;
+            if (s[i] == '1')
+                ones++;
         if (ones < k)
             return ans;
+
+        ones=0;    
         int st = 0;
         for (int i = 0; i < n; i++) {
-            string curr;
-             m[s[i]]++;
-             while(m['1']>k){
-                m[s[st]]--;
+            
+            if(s[i]=='1')ones++;
+            while (ones > k) {
+                if(s[st]=='1')ones--;
                 st++;
-             }
-            if (m['1'] == k) {
-               
-                    while(s[st]=='0'){
-                    m[s[st]]--;
-                    st++;
-                    }
+            }
 
-                
-                
+            if (ones == k) {
+                string curr;
+                while (s[st] == '0') {
+                    st++;
+                }
+
                 for (int j = st; j <= i; j++) {
                     curr.push_back(s[j]);
                 }
-                
-                if (ans.size() == 0 || curr.size() < ans.size()){
-                    ans = curr;}
-                
-                    
-                       else  if (curr.size()==ans.size()&&curr < ans) {
-                            ans = curr;
-                            
-                        }
-                    
+
+                if (ans.size() == 0 || curr.size() < ans.size()) {
+                    ans = curr;
+                }
+
+                else if (curr.size() == ans.size() && curr < ans) {
+                    ans = curr;
                 }
             }
-           
-        
+        }
+
         return ans;
     }
 };
