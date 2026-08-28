3be4dba0bf7412bb36f60ce5c5b15edce0f1fce7
Yash007-1 <yash116765@gmail.com>
2026-08-28 22:27:52 +0530

    Time: 1822 ms (5.01%), Space: 62.9 MB (8.79%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    bool valid(string&s,int i,int j){
-        if(i==j)return true;
-        
+    bool valid(string&s,int i,int j,vector<vector<int>>&dp){
+        if(i==j)return dp[i][j]=true;
+        if(dp[i][j]!=-1)return dp[i][j];
         while(i<j){
-            if(s[i]!=s[j])return false;
+            if(s[i]!=s[j])return dp[i][j]=false;
             i++;
             j--;
         }
-        return true;
+        return dp[i][j]=true;
     }
-  int f(string &s,int i,vector<int>&dp){
+  int f(string &s,int i,vector<int>&dp,vector<vector<int>>&palin){
    if(i==s.size())return dp[i]=-1;
    if(dp[i]!=-1)return dp[i];
    int ans=s.size();
    for(int k=i;k<s.size();k++){
     int right=s.size();
-   if(valid(s,i,k))right=1+f(s,k+1,dp);
+   if(valid(s,i,k,palin))right=1+f(s,k+1,dp,palin);
     ans=min(ans,right);
    }
    return dp[i]=ans;
@@ -24,6 +24,7 @@ public:
   }
     int minCut(string &s) {
        vector<int>dp(s.size()+1,-1);
-        return f(s,0,dp);
+       vector<vector<int>>palin(s.size(),vector<int>(s.size(),-1));
+        return f(s,0,dp,palin);
     }
 };
