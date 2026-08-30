2d89b9103ca319bc3dd8369fcdf4e2db9d8ed12e
Yash007-1 <yash116765@gmail.com>
2026-08-30 17:01:23 +0530

    Time: 0 ms (100%), Space: 92.1 MB (58.82%) - LeetHub

--- 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.cpp
+++ 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.cpp
@@ -13,42 +13,5 @@ public:
       b_del=max(n-mini,n-maxi);
       fb_del=min(mini+1+n-maxi,n-mini+1+maxi);
       return min(f_del,min(b_del,fb_del));
-       int last=0;
-       int i=0,j=n-1;
-       int del=0;
-       while(i<=j){
-        if(nums[i]==mini||nums[i]==maxi){
-            if(i-last<j-i){
-                 del+=i-last+1;
-                 last=i+1;
-                
-            }
-            else{
-                 del+=j-i+1;
-                j=j-i-1;
-               
-            }
-        }
-        i++;
-       }
-       i=n-1,j=0;
-       last=n-1;
-       int del2=0;
-       while(i>=j){
-        if(nums[i]==mini||nums[i]==maxi){
-            if(last-i<i-j){
-                 del2+=last-i+1;
-                 last=i-1;
-                
-            }
-            else{
-                 del2+=i-j+1;
-                j=i-j+1;
-               
-            }
-        }
-        i--;
-       }
-    return min(del,del2);}
-
+    }
 };
