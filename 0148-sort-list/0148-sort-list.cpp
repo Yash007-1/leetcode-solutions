805ceb199f2952c5afbcabf35c81a25924c30ef0
Yash007-1 <yash116765@gmail.com>
2026-09-03 23:19:09 +0530

    Time: 57 ms (12.3%), Space: 75.7 MB (32.03%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -10,20 +10,54 @@
  */
 class Solution {
 public:
+ListNode* middle(ListNode*head){
+    ListNode* slow=head;
+    ListNode* fast=head->next;
+    while(fast!=NULL&&fast->next!=NULL){
+        slow=slow->next;
+        fast=fast->next->next;
+    }
+    return slow;
+}
+ListNode*merge(ListNode*l1,ListNode*l2){
+    ListNode*res=new ListNode(-1);
+    ListNode*reshead=res;
+    
+    while(l1!=NULL&&l2!=NULL){
+        if(l1->val<l2->val){
+            res->next=l1;
+            l1=l1->next;
+        }
+        else{
+            res->next=l2;
+            l2=l2->next;
+        }
+        res=res->next;
+    }
+    while(l1!=NULL){
+        res->next=l1;
+        l1=l1->next;
+        res=res->next;
+    }
+    while(l2!=NULL){
+        res->next=l2;
+        l2=l2->next;
+        res=res->next;
+    }
+    return reshead->next;
+}
+ListNode*mergesort(ListNode*head){
+    if(head==NULL||head->next==NULL)return head;
+    ListNode* mid=middle(head);
+    ListNode* right=mid->next;
+    mid->next=NULL;
+    ListNode* left=mergesort(head);
+    ListNode*r=mergesort(right);
+    return merge(left,r);
+    
+}
     ListNode* sortList(ListNode* head) {
-        vector<int>arr;
-        ListNode* temp=head;
-        while(temp!=NULL){
-            arr.push_back(temp->val);
-            temp=temp->next;
-        }
-        sort(arr.begin(),arr.end());
-        temp=head;int i=0;
-        while(temp!=NULL){
-            temp->val=arr[i];
-            i++;
-            temp=temp->next;
-        }
-        return head;
+        if(head==NULL)return NULL;
+        return mergesort(head);
     }
 };
