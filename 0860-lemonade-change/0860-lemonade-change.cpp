f3972efa8d097196fd5f1c09c19b869818e13bbb
Yash007-1 <yash116765@gmail.com>
2026-09-04 14:33:28 +0530

    Time: 0 ms (100%), Space: 87.2 MB (44.79%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.cpp
+++ 0860-lemonade-change/0860-lemonade-change.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     bool lemonadeChange(vector<int>& bills) {
-        vector<int>arr(2,0);
+        int five=0,ten=0;
          
         for(int i=0;i<bills.size();i++){
                 int change=bills[i]-5;
                 
-                while(change>=10&&arr[1]>0){
-                    arr[1]--;
+                while(change>=10&&ten>0){
+                    ten--;
                     change-=10;
                 }
-                while(change>=5&&arr[0]>0){
-                    arr[0]--;
+                while(change>=5&&five>0){
+                    five--;
                     change-=5;
                 }
                 if(change!=0)return false;
-                if(bills[i]!=20)
-                arr[bills[i]/10]++;
+                if(bills[i]==10)ten++;
+                else if(bills[i]==5)five++;
 
              
         }
