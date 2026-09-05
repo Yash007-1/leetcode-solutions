4a285a69382fa034b643acd16370c550dd7a1893
Yash007-1 <yash116765@gmail.com>
2026-09-05 15:29:49 +0530

    Time: 0 ms (100%), Space: 14.1 MB (79.81%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -7,13 +7,11 @@ public:
         while(st<=end){
             int mid=(st+end)/2;
             ans=min(ans,nums[mid]);
-            if(nums[st]>nums[end]){
+            
                 if(nums[end]<nums[mid])st=mid+1;
                  else end=mid-1;
-            }
+            
 
-            else{ if(nums[st]<nums[mid])end=mid-1;
-            else st=mid+1;}
         }
     return ans;}
 };
