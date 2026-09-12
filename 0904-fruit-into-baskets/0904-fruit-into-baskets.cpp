9714de8ab2a0e080347660fb6f2a15459077d8f2
Yash007-1 <yash116765@gmail.com>
2026-09-12 16:32:51 +0530

    Time: 1 ms (97.94%), Space: 79.8 MB (94.89%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
     int totalFruit(vector<int>& nums) {
-        int f1=-1,f2=-1;
+        int lastf=-1,f2=-1;
         int n=nums.size();
-        int i=0,r=0;
+        int r=0;
+        int currcount=1,streak=0;
         int ans=0;
-        while(r<n&&i<n){
-           if(f1==-1)f1=nums[r];
-           
-           else if(nums[r]!=f1&&f2==-1)f2=nums[r];
-           else if(nums[r]!=f1&&nums[r]!=f2){
-           
-                i=r-1;
-                while(i>=0&&nums[i]==nums[r-1])i--;
-                i++;
-                 f1=nums[i];
-                f2=nums[r];
-                
-
-            }
-            ans=max(ans,r-i+1);
+        while(r<n){
+           if(nums[r]==lastf||nums[r]==f2)currcount++;
+           else currcount=streak+1;
+           if(nums[r]==lastf)streak++;
+           else {
+            streak=1;
+            f2=lastf;
+            lastf=nums[r];
+           }
+            ans=max(ans,currcount);
            
            r++;}
         
