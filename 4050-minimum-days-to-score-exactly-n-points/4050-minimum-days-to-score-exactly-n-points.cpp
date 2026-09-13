d4c427c826d6b971c507799c97929ad9d47a70d8
Yash007-1 <yash116765@gmail.com>
2026-09-13 11:13:04 +0530

    Time: 218 ms (40%), Space: 15.5 MB (40%) - LeetHub

--- 4050-minimum-days-to-score-exactly-n-points/4050-minimum-days-to-score-exactly-n-points.cpp
+++ 4050-minimum-days-to-score-exactly-n-points/4050-minimum-days-to-score-exactly-n-points.cpp
@@ -5,12 +5,12 @@ public:
        dp[0]=0;
         for(int score=0;score<=n;score++){
            long long sum=0;
-           for(int i=0;i<=score;i++){
+           for(int i=1;i<=score;i++){
                 sum+=i;
                 if(sum>score)break;
                 int remaining=score-sum;
                 if(remaining==0)dp[score]=min(dp[score],i);
-                else if(dp[remaining]!=INT_MAX){
+                else {
                     dp[score]=min(dp[score],dp[remaining]+i+1);
                 }
            }
