5d2de14a8c618853693cf44d2a131d438372eb1d
Yash007-1 <yash116765@gmail.com>
2026-09-13 11:05:23 +0530

    Time: 253 ms (0%), Space: 15.5 MB (40%) - LeetHub

---
+++ 4050-minimum-days-to-score-exactly-n-points/4050-minimum-days-to-score-exactly-n-points.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minDays(int n) {
+        vector<int>dp(n+1,INT_MAX);
+       dp[0]=0;
+        for(int score=0;score<=n;score++){
+           long long sum=0;
+           for(int i=0;i<=score;i++){
+                sum+=i;
+                if(sum>score)break;
+                int remaining=score-sum;
+                if(remaining==0)dp[score]=min(dp[score],i);
+                else if(dp[remaining]!=INT_MAX){
+                    dp[score]=min(dp[score],dp[remaining]+i+1);
+                }
+           }
+            
+        }
+   return dp[n];}
+};
