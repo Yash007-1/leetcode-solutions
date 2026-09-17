e903ffcfbb1b1da3fd36fe1b19cf19cc6ce5da60
Yash007-1 <yash116765@gmail.com>
2026-09-17 16:21:21 +0530

    Time: 5 ms (81.7%), Space: 88.9 MB (79.38%) - LeetHub

--- 1477-find-two-non-overlapping-sub-arrays-each-with-target-sum/1477-find-two-non-overlapping-sub-arrays-each-with-target-sum.cpp
+++ 1477-find-two-non-overlapping-sub-arrays-each-with-target-sum/1477-find-two-non-overlapping-sub-arrays-each-with-target-sum.cpp
@@ -1,39 +1,28 @@
 class Solution {
 public:
     int minSumOfLengths(vector<int>&nums, int target) {
-        int l1=1e9,l2=1e9;
-        int x1=-1,x2=-1;
+       
         int l=0,n=nums.size();
         long long sum=0;
-        if(nums[0]==1&&nums[1]==1&&nums[4]==2&&nums[7]==4&&nums[6]==4)return 6;
-        if(nums[0]==2&&nums[1]==2&&nums[4]==4&&nums[7]==1&&nums[6]==4)return 23;
+        int ans=1e9;
+        vector<int>best(n,1e9);
         for(int r=0;r<n;r++){
             sum+=nums[r];
             while(l<r&&sum>target){
                 sum-=nums[l];
                 l++;
             }
+            if(r>0)best[r]=best[r-1];
             if(sum==target){
-                if(l1>r-l+1){
-                    x2=x1;
-                    l2=l1;
-                    l1=r-l+1;
-                    x1=r;
-                    
-                }
-                else if(l2>r-l+1&&l>x1&&l>x2){
-                   l2=r-l+1;
-                   x2=r;
-                     
-                }
+                int len=r-l+1;
+                if(l>0&&best[l-1]!=1e9)
+               ans=min(ans,len+best[l-1]);
+              
+              best[r]=min(best[r],len);
                 
                 
                
             }
         }
-    if(l2==1e9){
-        return -1;
-        
-    }
-    return l1+l2;}
+    return ans==1e9?-1:ans;;}
 };
