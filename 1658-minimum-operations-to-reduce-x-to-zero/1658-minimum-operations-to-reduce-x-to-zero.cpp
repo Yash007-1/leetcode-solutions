8970fcc866205b77e808b129b12129b440f21691
Yash007-1 <yash116765@gmail.com>
2026-09-23 10:58:39 +0530

    Time: 645 ms (5.01%), Space: 200.8 MB (5.01%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -1,21 +1,8 @@
 class Solution {
 public:
-int f(vector<int>&nums,int i,int j,int x,vector<vector<vector<int>>>&dp){
-    if(x==0)return 0;
-    if(x<0)return 1e5;
-    if(j<i)return 1e5;
-    if(dp[i][j][x]!=-1)return dp[i][j][x];
-    int begin=1+f(nums,i+1,j,x-nums[i],dp);
-    int end=1+f(nums,i,j-1,x-nums[j],dp);
-    return dp[i][j][x]=min(begin,end);
-
-}
     int minOperations(vector<int>& nums, int x) {
         int n=nums.size();
-        // vector<vector<vector<int>>>dp(n,vector<vector<int>>(n,vector<int>(x+1,-1)));
-        // int ans=f(nums,0,nums.size()-1,x,dp);
-        // return ans>=1e5?-1:ans;
-
+       
         
     map<int,int>presum;
     int s=0;
@@ -41,17 +28,13 @@ int f(vector<int>&nums,int i,int j,int x,vector<vector<vector<int>>>&dp){
         }
 
     }
-    //  int prefind=presum.find(x)!=presum.end();
-    //  int postfind=postsum.find(x)!=postsum.end();
-    //  if(prefind&&postfind)return min(presum[x],postsum[x]);
-    //  if(prefind)return presum[x];
-    //  if(postfind)return postsum[x];
+  
 int ans=1e9;
 s=0;
-int pre=0;
+
     for(int i=0;i<n;i++){
         s+=nums[i];
-        int req=x-s;   if(req<0){pre=i;break;}
+        int req=x-s;   if(req<0)break;
         if(req==0){
             ans=min(ans,i+1);
         }
